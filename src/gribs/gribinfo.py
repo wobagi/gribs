f2bef51e61aeedebd6c79e2dc8a3b1c9a8d295b4
wobagi <4607722-wobagi@users.noreply.gitlab.com>
2021-11-19 22:53:29 +0100

    gribinfo

---
+++ src/gribs/gribinfo.py
@@ -0,0 +1,14 @@
+import argparse
+import pathlib
+from gribs.gribmapper import GribMapper
+
+def cli():
+    parser = argparse.ArgumentParser(description="List grib files contents to stdout")
+    parser.add_argument("source", type=pathlib.Path, help="Source file")
+    args = parser.parse_args()
+
+    gm = GribMapper(str(args.source))
+    gm.list()
+
+if __name__=="__main__":
+    cli()
