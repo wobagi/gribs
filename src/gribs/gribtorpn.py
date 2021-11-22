830088df84ab53bf9798a29f6d65f0a19d9d8252
wobagi <wobagi@gmail.com>
2021-11-22 21:59:02 +0100

    Display short options first

--- src/gribs/gribtorpn.py
+++ src/gribs/gribtorpn.py
@@ -18,9 +18,9 @@ def convert(path, args):
 def cli():
     parser = argparse.ArgumentParser(description="Convert grib files to rpn format")
     parser.add_argument("source", nargs="+", type=pathlib.Path, help="List of dirs and/or grib files")
-    parser.add_argument("--target", "-t", type=pathlib.Path, required=True, help="Target rpn file")
-    parser.add_argument("--glob", "-g", type=str, default="*.grib?", help="Glob string applied to all specified source dirs. Does not affect individual files specified explicitly. Defaults to '*.grib?' to match both grib and grib2 extension.")
-    parser.add_argument("--etiket", "-e", type=str, default="G092V3N", help="Set etiket column value for resulting rpn file.")
+    parser.add_argument("-t", "--target", type=pathlib.Path, required=True, help="Target rpn file")
+    parser.add_argument("-g", "--glob", type=str, default="*.grib?", help="Glob string applied to all specified source dirs. Does not affect individual files specified explicitly. Defaults to '*.grib?' to match both grib and grib2 extension.")
+    parser.add_argument("-e", "--etiket", type=str, default="G092V3N", help="Set etiket column value for resulting rpn file.")
     parser.add_argument("-o", "--overwrite", action="store_true", help="Overwrite target file if exists. Does not make much sense when processing multiple files.")
     parser.add_argument("-v", "--verbose", action="store_true", help="Display FSTD output")
     args = parser.parse_args()
