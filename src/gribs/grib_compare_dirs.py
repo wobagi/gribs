e448cbd3b0115b27ded72c2617f07293817f6492
wobagi <wobagi@gmail.com>
2021-12-02 11:43:22 +0100

    String minor updates

--- src/gribs/grib_compare_dirs.py
+++ src/gribs/grib_compare_dirs.py
@@ -21,18 +21,18 @@ def diff(dir1, dir2):
     stems2 = [re.match(".+(CMC.+)_\d{10}_", name).group(1) for name in cmc_files2]
 
     if set(stems1)==set(stems2):
-        print("Both directories contain matching sets of data.")
+        print("Both directories contain matching layers.")
     else:
         dif1 = sorted(set(stems1)-set(stems2))
         dif2 = sorted(set(stems2)-set(stems1))
         print("Found differences between directories")
         if dif1:
-            print(f"Dir 1 unmatched files:{_n}{_n.join(dif1)}")
+            print(f"Dir 1 unmatched layers:{_n}{_n.join(dif1)}")
         if dif2:
-            print(f"Dir 2 unmatched files:{_n}{_n.join(dif2)}")
+            print(f"Dir 2 unmatched layers:{_n}{_n.join(dif2)}")
 
 def cli():
-    parser = argparse.ArgumentParser(description="Diff two dirs in terms of CMC grib file types")
+    parser = argparse.ArgumentParser(description="Diff two dirs in terms of CMC grib layers")
     parser.add_argument("dir1", type=Path, help="First dir")
     parser.add_argument("dir2", type=Path, help="Second dir")
     args = parser.parse_args()
