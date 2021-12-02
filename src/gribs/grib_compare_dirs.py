f0a8e0bc06f085ab59d6523119f493b668f06655
wobagi <wobagi@gmail.com>
2021-12-02 11:38:51 +0100

    Working version

--- src/gribs/grib_compare_dirs.py
+++ src/gribs/grib_compare_dirs.py
@@ -2,6 +2,7 @@
 
 import re
 import math
+from pathlib import Path
 import argparse
 import datetime as dt
 
@@ -11,24 +12,27 @@ from glob import glob
 from gribs.gribmapper import GribMapper
 
 def diff(dir1, dir2):
-    cmc_files1 = [f for f in dir1.glob("*.grib?")]
-    cmc_files2 = [f for f in dir2.glob("*.grib?")]
+    cmc_files1 = [str(f) for f in dir1.glob("*.grib?")]
+    cmc_files2 = [str(f) for f in dir2.glob("*.grib?")]
 
-    # "CMC_glb_WTMP_SFC_0_latlon.15x.15_2021120200_P000.grib2"
+    stems1 = [re.match(".+(CMC.+)_\d{10}_", name).group(1) for name in cmc_files1]
+    stems2 = [re.match(".+(CMC.+)_\d{10}_", name).group(1) for name in cmc_files2]
 
-    stems1 = [re.match("(CMC.+)_\d{10}_.+\.grib2", name).group(1) for name in cmc_files1]
-    stems2 = [re.match("(CMC.+)_\d{10}_.+\.grib2", name).group(1) for name in cmc_files2]
-
-    dif1 = sorted(set(stems1)-set(stems2))
-    dif2 = sorted(set(stems2)-set(stems1))
-
-    print(f"Dir 1:\n{'\n'.join(dif1)}")
-    print(f"Dir 2:\n{'\n'.join(dif2)}")
+    if set(stems1)==set(stems2):
+        print("Both directories contain matching sets of data.")
+    else:
+        dif1 = sorted(set(stems1)-set(stems2))
+        dif2 = sorted(set(stems2)-set(stems1))
+        print("Found differences between directories")
+        if dif1:
+            print(f"Dir 1 unmatched files:{nl}{nl.join(dif1)}")
+        if dif2:
+            print(f"Dir 2 unmatched files:{nl}{nl.join(dif2)}")
 
 def cli():
     parser = argparse.ArgumentParser(description="Diff two dirs in terms of CMC grib file types")
-    parser.add_argument("dir1", type=pathlib.Path, help="First dir")
-    parser.add_argument("dir2", type=pathlib.Path, help="Second dir")
+    parser.add_argument("dir1", type=Path, help="First dir")
+    parser.add_argument("dir2", type=Path, help="Second dir")
     args = parser.parse_args()
 
     diff(args.dir1, args.dir2)
