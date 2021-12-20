95bc1f920203ed84e3b352e8c976fb0caf85e879
wobagi <4607722-wobagi@users.noreply.gitlab.com>
2021-12-20 19:00:53 +0100

    Added color and other args groups

--- src/gribs/gribtorpn.py
+++ src/gribs/gribtorpn.py
@@ -4,26 +4,41 @@ import argparse
 
 from gribs.gribmapper import GribMapper
 
+RGB_G = "\u001b[32m" 
+RGB_B = "\u001b[34m"
+RGB_0 = "\u001b[0m"
+
 def convert(path, args):
-    gm = GribMapper(str(path), ip_oldstyle=args.oldstyle)
-    gm.verbose = args.verbose
-    gm.etiket = args.etiket
-    if gm.is_required():
-        print(f"{str(path.name)} - Converting ...")
-        gm.to_rpn(target=args.target, overwrite=args.overwrite)
-    else:
-        print(f"{str(path.name)} -",
-              f"{gm.gribvar} @ {gm._level_type} ({gm._level}) is missing from GribMapper dictionary.")
+    CR = RGB_G if args.color else RGB_0
+    CG = RGB_B if args.color else RGB_0
+    C0 = RGB_0
+
+    for gm in GribMapper.from_path(str(path)):
+        gm.etiket = args.etiket
+        if gm.is_recognized():
+            print(f"{CR}... {str(path.name)} - Converting {gm.gribvar} @ {gm._level_type} ({gm._level}){C0}")
+            if not args.dry:
+                gm.to_rpn(target=args.target,
+                          overwrite=args.overwrite,
+                          ip_oldstyle=args.oldstyle,
+                          verbose=args.verbose)
+        else:
+            print(f"{CG}??? {str(path.name)} -",
+                f"{gm.gribvar} @ {gm._level_type} ({gm._level}) is missing from GribMapper dictionary.{C0}")
 
 def cli():
     parser = argparse.ArgumentParser(description="Convert grib files to rpn format")
     parser.add_argument("source", nargs="+", type=pathlib.Path, help="List of dirs and/or grib files")
     parser.add_argument("-t", "--target", type=pathlib.Path, required=True, help="Target rpn file")
     parser.add_argument("-g", "--glob", type=str, default="*.grib?", help="Glob string applied to all specified source dirs. Does not affect individual files specified explicitly. Defaults to '*.grib?' to match both grib and grib2 extension.")
-    parser.add_argument("-e", "--etiket", type=str, default="G092V3N", help="Set etiket column value for resulting rpn file.")
-    parser.add_argument("-o", "--overwrite", action="store_true", help="Overwrite target file if exists. Does not make much sense when processing multiple files.")
-    parser.add_argument("--oldstyle", action="store_true", help="Get IP1 code values in oldstyle format.")
-    parser.add_argument("-v", "--verbose", action="store_true", help="Display FSTD output")
+    group1 = parser.add_argument_group('fstd options')
+    group1.add_argument("-e", "--etiket", type=str, default="G092V3N", help="Set etiket column value for resulting rpn file.")
+    group1.add_argument("-o", "--overwrite", action="store_true", help="Overwrite target file if exists. Does not make much sense when processing multiple files.")
+    group1.add_argument("--oldstyle", action="store_true", help="Get IP1 code values in oldstyle format.")
+    group1.add_argument("-v", "--verbose", action="store_true", help="Display FSTD output")
+    group2 = parser.add_argument_group('other options')
+    group2.add_argument("-d", "--dry", action="store_true", help="Dry run with no actual file saving")
+    group2.add_argument("-c", "--color", action="store_true", help="Use color for messages")
     args = parser.parse_args()
 
     for pt in args.source:
