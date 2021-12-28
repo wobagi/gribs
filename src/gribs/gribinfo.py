a46a25aff8053285ab0b61536534f7b94a8355e7
wobagi <4607722-wobagi@users.noreply.gitlab.com>
2021-12-28 10:33:12 +0100

    Gribinfo update to match gribmapper.from_path generator

--- src/gribs/gribinfo.py
+++ src/gribs/gribinfo.py
@@ -7,8 +7,8 @@ def cli():
     parser.add_argument("source", type=pathlib.Path, help="Source file")
     args = parser.parse_args()
 
-    gm = GribMapper.from_path(str(args.source))
-    gm.list()
+    for gm in GribMapper.from_path(str(args.source)):
+        gm.list()
 
 if __name__=="__main__":
     cli()
